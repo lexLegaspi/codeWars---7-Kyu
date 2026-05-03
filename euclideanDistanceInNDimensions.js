77dfc5c1f48c100f533be541e7df367333bfd0ff
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-03 23:07:24 +0900

    Update euclideanDistanceInNDimensions.js

--- euclideanDistanceInNDimensions.js
+++ euclideanDistanceInNDimensions.js
@@ -1,4 +1,12 @@
-// Implement a function to calculate the distance between two points in n-dimensional space. 
+// Implement a function to calculate the distance between two points in n-dimensional space.
 // The two points will be passed to your function as arrays of the same length.
 
 // My Solution:
+
+function euclideanDistance(point1, point2) {
+  let differences = [];
+  for (let i = 0; i < point1.length; i++) {
+    differences.push(Math.pow(point1[i] - point2[i], 2));
+  }
+  return Math.sqrt(differences.reduce((a, b) => a + b, 0));
+}

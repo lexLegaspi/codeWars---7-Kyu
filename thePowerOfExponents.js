388da1465bc3a5f3212b91a20c3fc70c8c528306
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-03 23:34:54 +0900

    Create thePowerOfExponents.js

---
+++ thePowerOfExponents.js
@@ -0,0 +1,11 @@
+// This kata is based on: Exponent Method
+
+// Create a method called power that takes two integers and returns the value of the first argument raised to the power of the second.
+
+// Unlike the Exponent Method, you should also take in account negative exponents.
+
+// Your solution won't be tested against special cases leading to undefined values (0**-2 for example)
+
+// Note: The ** operator (JS: Math.pow) has been disabled.
+
+// My Solution:

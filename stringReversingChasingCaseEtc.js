50c2d63b559c7d9e21030634bfd0444aac969e2b
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-06 23:27:12 +0900

    Update stringReversingChasingCaseEtc.js

--- stringReversingChasingCaseEtc.js
+++ stringReversingChasingCaseEtc.js
@@ -2,9 +2,23 @@
 
 // the reverse of the 2nd string with inverted case; e.g Fish -> HSIf
 // a separator in between both strings: @@@
-// the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER 
+// the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER
 // ** Keep in mind that this kata was initially designed for Shell, I'm aware it may be easier in other languages.**
 
-
-
 // My Solution:
+
+function reverseAndMirror(s1, s2) {
+  s2 = s2
+    .split("")
+    .map((x) => (x.charCodeAt(0) < 97 ? x.toLowerCase() : x.toUpperCase()))
+    .reverse()
+    .join("");
+
+  s1 = s1
+    .split("")
+    .map((x) => (x.charCodeAt(0) < 97 ? x.toLowerCase() : x.toUpperCase()));
+
+  let s1Rev = [...s1].reverse();
+
+  return s2 + "@@@" + s1Rev.concat(s1).join("");
+}

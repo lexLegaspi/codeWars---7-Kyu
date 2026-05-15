2dcb5223e8f2ab684b1eac0abd6fe1545aec5746
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-15 23:38:09 +0900

    Create ultimateArrayReverser.js

---
+++ ultimateArrayReverser.js
@@ -0,0 +1,10 @@
+// Description:
+// Task
+// Given an array of strings, reverse them and their order in such a way that their lengths stays the same as the lengths of the original inputs.
+
+// Example:
+// Input:  {"I", "like", "big", "butts", "and", "I", "cannot", "lie!"}
+// Output: {"!", "eilt", "onn", "acIdn", "ast", "t", "ubgibe", "kilI"}
+// Good luck!
+
+// My Soluttion:

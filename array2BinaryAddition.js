0082eb24ee3ef3aa8a6641a4d5547ec609b05b9f
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-17 22:54:15 +0900

    Create array2BinaryAddition.js

---
+++ array2BinaryAddition.js
@@ -0,0 +1,10 @@
+// Given an array containing only integers, add all the elements and return the binary equivalent of that sum.
+
+// If the array contains any non-integer element (e.g. an object, a float, a string and so on), return false.
+
+// Note: The sum of an empty array is zero.
+
+// [1, 2]      --> "11"
+// [1, "a", 2] --> false / False (depending on the language)
+
+// My Solution:

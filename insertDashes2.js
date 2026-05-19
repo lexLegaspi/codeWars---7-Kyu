8f942337287dedcbb7dc19b01e503da4e68baeb3
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-19 23:04:14 +0900

    Update insertDashes2.js

--- insertDashes2.js
+++ insertDashes2.js
@@ -9,3 +9,33 @@
 // Note: Digit zero ('0') is not considered when inserting dashes or asterisks.
 
 // My Solution:
+
+function insertDashII(num) {
+  //code me
+  num = String(num);
+  const isOdd = (n) => n % 2 !== 0;
+  const isEven = (n) => n % 2 === 0;
+  let result = "";
+  for (let i = 0; i < num.length; i++) {
+    if (
+      isOdd(+num[i]) &&
+      isOdd(+num[i + 1]) &&
+      i + 1 < num.length &&
+      +num[i + 1] !== 0 &&
+      +num[i] !== 0
+    ) {
+      result += num[i] + "-";
+    } else if (
+      isEven(+num[i]) &&
+      isEven(+num[i + 1]) &&
+      i + 1 < num.length &&
+      +num[i + 1] !== 0 &&
+      +num[i] !== 0
+    ) {
+      result += num[i] + "*";
+    } else {
+      result += num[i];
+    }
+  }
+  return result;
+}

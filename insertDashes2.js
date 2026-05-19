e787a92318100202a84bdf002a55399b6a20c85c
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-19 23:01:24 +0900

    Create insertDashes2.js

---
+++ insertDashes2.js
@@ -0,0 +1,11 @@
+// This is a follow up from my kata Insert Dashes.
+
+// Write a function that takes a non-negative integer, insert dashes ('-') between each two odd digits and insert asterisks ('*') between each two nonzero even digits.
+
+// For example:
+
+// 454793 --> "4547-9-3"
+// 1012356895 --> "10123-56*89-5"
+// Note: Digit zero ('0') is not considered when inserting dashes or asterisks.
+
+// My Solution:

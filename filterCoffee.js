f78d61089620417c2b4e25231d9e0b2e66b3a363
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-05-27 22:20:41 +0900

    Update filterCoffee.js

--- filterCoffee.js
+++ filterCoffee.js
@@ -9,3 +9,11 @@
 // The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
 
 // My Solution:
+
+function search(budget, prices) {
+  // return array of prices that are within budget
+  return prices
+    .filter((x) => x <= budget)
+    .sort((a, b) => a - b)
+    .join(",");
+}

4a3f9a5a5069b500c59b8d3225a1b272a022b2ff
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-06-06 22:34:28 +0900

    Update symbolsCounted.js

--- symbolsCounted.js
+++ symbolsCounted.js
@@ -3,3 +3,22 @@
 // Example: abbreviation => a2b2revi2ton
 
 // My Solution:
+
+const transform = (s) => {
+  let counts = {};
+  let result = "";
+  for (let i = 0; i < s.length; i++) {
+    if (!counts[s[i]]) {
+      counts[s[i]] = s.split(s[i]).length - 1;
+    }
+    if (counts[s[i]] < 2 && !result.includes(s[i])) {
+      result += s[i];
+    } else {
+      if (!result.includes(s[i])) {
+        result += s[i] + counts[s[i]];
+      }
+    }
+  }
+
+  return result;
+};

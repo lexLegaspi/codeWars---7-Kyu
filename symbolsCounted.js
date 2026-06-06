684919e88d341c79c2ece77ee7a2e9d07bd4b945
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-06-06 22:34:05 +0900

    Create symbolsCounted.js

---
+++ symbolsCounted.js
@@ -0,0 +1,5 @@
+// In this kata you will have to transform each string so that it contains count for every symbol it contains, starting from 2. The order of symbols should be preserved.
+
+// Example: abbreviation => a2b2revi2ton
+
+// My Solution:

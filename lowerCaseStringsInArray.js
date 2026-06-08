27ee25c387bd0fc15f5256f07a2e5139abbc9492
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-06-08 22:41:56 +0900

    Create lowerCaseStringsInArray.js

---
+++ lowerCaseStringsInArray.js
@@ -0,0 +1,7 @@
+// Create a function to lowercase all strings in an array. Non-string items should remain unchanged.
+
+// Example
+// arrayLowerCase(['Red', 'Green']) == ['red', 'green']
+// arrayLowerCase([1, 'Green']) == [1, 'green']
+
+// My Solution:

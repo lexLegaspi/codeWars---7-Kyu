b199b6b50de895cda24490bf0070009b01a57ea3
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-06-18 23:47:39 +0900

    Update slaphead.js

--- slaphead.js
+++ slaphead.js
@@ -1,16 +1,36 @@
-Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
+// Being a bald man myself, I know the feeling of needing to keep it clean shaven. Nothing worse that a stray hair waving in the wind.
 
-You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
+// You will be given a string(x). Clean shaved head is shown as "-" and stray hairs are shown as "/". Your task is to check the head for stray hairs and get rid of them.
 
-You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
+// You should return the original string, but with any stray hairs removed. Keep count ot them though, as there is a second element you need to return:
 
-0 hairs --> "Clean!"
-1 hair --> "Unicorn!"
-2 hairs --> "Homer!"
-3-5 hairs --> "Careless!"
->5 hairs --> "Hobo!"
+// 0 hairs --> "Clean!"
+// 1 hair --> "Unicorn!"
+// 2 hairs --> "Homer!"
+// 3-5 hairs --> "Careless!"
+// >5 hairs --> "Hobo!"
 
-So for this head: "------/------" you shoud return:
+// So for this head: "------/------" you shoud return:
 
-["-------------", "Unicorn"]
+// ["-------------", "Unicorn"]
 
+// My Solution:
+
+function bald(x) {
+  let count = x.split("/").length - 1;
+  let result = x.split("/").join("");
+  let value = "";
+
+  if (count === 0) {
+    value = "Clean!";
+  } else if (count === 1) {
+    value = "Unicorn!";
+  } else if (count === 2) {
+    value = "Homer!";
+  } else if (count >= 3 && count <= 5) {
+    value = "Careless!";
+  } else {
+    value = "Hobo!";
+  }
+  return [x.replaceAll("/", "-"), value];
+}

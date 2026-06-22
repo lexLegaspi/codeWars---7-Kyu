43cb3b901960babe909ceeaffd5169aa3cf48701
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-06-22 21:38:32 +0900

    Update isItALetter.js

--- isItALetter.js
+++ isItALetter.js
@@ -1,3 +1,9 @@
 // Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
 
 // My Solution:
+
+function isItLetter(character) {
+  return character.toLowerCase() >= "a" && character.toLowerCase() <= "z"
+    ? true
+    : false;
+}

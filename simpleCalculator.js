fa6bcb4791791ac4b89cc246217aa0db402e66f0
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-06-25 22:06:15 +0900

    Update simpleCalculator.js

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,32 +1,51 @@
-You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
+// You are required to create a simple calculator that returns the result of addition, subtraction, multiplication or division of two numbers.
 
-Your function will accept three arguments:
-The first and second argument should be numbers.
-The third argument should represent a sign indicating the operation to perform on these two numbers.
+// Your function will accept three arguments:
+// The first and second argument should be numbers.
+// The third argument should represent a sign indicating the operation to perform on these two numbers.
 
-You should return the result of applying the given operation to these numbers.
+// You should return the result of applying the given operation to these numbers.
 
-Note: In dynamically typed languages (JS, PHP, Python), the first and second arguments can be not numbers. In that case, return "unknown value".
+// Note: In dynamically typed languages (JS, PHP, Python), the first and second arguments can be not numbers. In that case, return "unknown value".
 
-If the given operation to perform on the two numbers is not one of the four mentioned above, you should:
+// If the given operation to perform on the two numbers is not one of the four mentioned above, you should:
 
-return a value:
-"unknown value" (JS, PHP, Python)
-throw an exception:
-std::invalid_argument (C++)
-ArgumentException (C#)
-IllegalArgumentException (Java)
-Example:
-arguments: 1, 2, "+"
-should return 3
+// return a value:
+// "unknown value" (JS, PHP, Python)
+// throw an exception:
+// std::invalid_argument (C++)
+// ArgumentException (C#)
+// IllegalArgumentException (Java)
+// Example:
+// arguments: 1, 2, "+"
+// should return 3
 
-arguments: 1, 2, "&"
-refer to the description for what you should return in this case
+// arguments: 1, 2, "&"
+// refer to the description for what you should return in this case
 
-# Specifically for dynamically-typed languages:
+// # Specifically for dynamically-typed languages:
 
-arguments: 1, "k", "*"
-should return "unknown value"
-Good luck!
+// arguments: 1, "k", "*"
+// should return "unknown value"
+// Good luck!
 
-My Solution:
+// My Solution:
+
+
+function calculator(a,b,sign){
+  // Your code here...
+  let op = '+-/*'
+  
+  if(!Number.isInteger(a) || !Number.isInteger(b) || !op.includes(sign)) return "unknown value"
+
+  switch(sign) {
+      case '+' : return a+b;
+      break;
+      case '-' : return a-b;
+      break;
+      case '*' : return a*b;
+      break;
+      case '/' : return a/b;
+      break;
+  }
+}

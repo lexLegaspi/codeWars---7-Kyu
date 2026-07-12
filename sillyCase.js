ff92fae7affd9212b2b7a421b2c656e79236471a
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-07-12 22:58:10 +0900

    Update sillyCase.js

--- sillyCase.js
+++ sillyCase.js
@@ -4,9 +4,15 @@
 
 // If it is an odd number then 'round' it up to find which letters to uppercase. See example below.
 
-// sillycase("brian")  
-// //         --^-- midpoint  
-// //         bri    first half (lower-cased)  
-// //            AN second half (upper-cased)  
+// sillycase("brian")
+// //         --^-- midpoint
+// //         bri    first half (lower-cased)
+// //            AN second half (upper-cased)
 
 // My Solution:
+
+function sillycase(silly) {
+  //your code
+  let half = Math.round(silly.length / 2);
+  return silly.slice(0, half).toLowerCase() + silly.slice(half).toUpperCase();
+}

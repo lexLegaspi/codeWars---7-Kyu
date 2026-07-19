dede726edcdb41ae0253fe757ddd728adcbc9ae2
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-07-19 21:17:14 +0900

    Update ch4113ng3.js

--- ch4113ng3.js
+++ ch4113ng3.js
@@ -1,4 +1,12 @@
 // Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
 
-
 // My Solution:
+
+function nerdify(txt) {
+  return txt
+    .replaceAll("a", "4")
+    .replaceAll("e", "3")
+    .replaceAll("l", "1")
+    .replaceAll("A", "4")
+    .replaceAll("E", "3");
+}

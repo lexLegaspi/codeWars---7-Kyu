8bb359e6e3b4a704b77022c0d2990d8f32eabaa4
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-07-26 22:59:39 +0900

    Update whatComesAfter.js

--- whatComesAfter.js
+++ whatComesAfter.js
@@ -12,3 +12,18 @@
 // Adapted from: Ruby Kickstart
 
 // My Solution:
+
+function comes_after(str, l) {
+  //code
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i].toLowerCase() === l &&
+      str[i + 1] &&
+      str[i + 1].toUpperCase() !== str[i + 1].toLowerCase()
+    ) {
+      result += str[i + 1];
+    }
+  }
+  return result;
+}

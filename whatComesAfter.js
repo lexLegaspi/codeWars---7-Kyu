fe8c2078f5ec1956050d337367480008a9058810
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-07-26 22:58:34 +0900

    Create whatComesAfter.js

---
+++ whatComesAfter.js
@@ -0,0 +1,14 @@
+// You will be given two inputs: a string of words and a letter. For each string, return the alphabetic character after every instance of letter(case insensitive).
+
+// If there is a number, punctuation or underscore following the letter, it should not be returned.
+
+// If letter = 'r':
+// "are you really learning Ruby?" # => "eenu"
+// "Katy Perry is on the radio!"   # => "rya"
+// "Pirates say arrrrrrrrr."       # => "arrrrrrrr"
+// "r8 your friend"                # => "i"
+// Return an empty string if there are no instances of letter in the given string.
+
+// Adapted from: Ruby Kickstart
+
+// My Solution:

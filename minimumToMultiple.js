ae4f97675ad570cd1dd9a85ead4095ebb923dd87
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-07 23:08:06 +0900

    Update minimumToMultiple.js

--- minimumToMultiple.js
+++ minimumToMultiple.js
@@ -11,3 +11,16 @@
 // 1 <= x <= 105
 
 // My Solution:
+
+function minimum(a, x) {
+  // your solution here
+  if (a === x) return 0;
+  console.log(a + " " + x);
+  let i = 1;
+
+  while (true) {
+    if ((a + i) % x == 0) return i;
+    if ((a - i) % x == 0) return i;
+    i++;
+  }
+}

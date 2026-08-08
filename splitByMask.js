a3d3844ed47c2c51c4ee39df225e522792b9f35a
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-08 23:18:47 +0900

    Create splitByMask.js

---
+++ splitByMask.js
@@ -0,0 +1,13 @@
+// Given a string and a mask (a list of lengths), split the string into its parts accordingly.
+
+// Examples:
+// |     String      |  Mask (lengths)  |          Output           |
+// |-----------------|------------------|---------------------------|
+// |  "1234567890"   |  (3, 3, 4)       |  ["123", "456", "7890"]   |
+// |  "codewars"     |  (4, 4)          |  ["code", "wars"]         |
+// Notes:
+// The mask only contains strictly positive integers.
+// A mask is valid if and only if the sum of the lengths is equal to the length of the string.
+// Otherwise, return None, Nothing or a similar empty value.
+
+// My Solution:

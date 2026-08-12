8d6a6f3cf220cfc5cd823a3f84395c1c30d18e1b
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-12 22:00:25 +0900

    Update arrayComparator.js

--- arrayComparator.js
+++ arrayComparator.js
@@ -1,5 +1,15 @@
-// You have two arrays in this kata, every array contains unique elements only. 
-// Your task is to calculate number of elements in the first array which are 
+// You have two arrays in this kata, every array contains unique elements only.
+// Your task is to calculate number of elements in the first array which are
 // also present in the second array.
 
 // My Solution:
+
+function matchArrays(v, r) {
+  let count = 0;
+
+  for (let el of v) {
+    if (r.includes(el)) count++;
+  }
+
+  return count;
+}

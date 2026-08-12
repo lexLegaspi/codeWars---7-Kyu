d9c84ccfccbf25eb908d8386141a5d2ec4f7f14c
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-12 22:00:13 +0900

    .

---
+++ arrayComparator.js
@@ -0,0 +1,5 @@
+// You have two arrays in this kata, every array contains unique elements only. 
+// Your task is to calculate number of elements in the first array which are 
+// also present in the second array.
+
+// My Solution:

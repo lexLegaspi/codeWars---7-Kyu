120cf1934ec65a1095ff09fbd16328066042c0b1
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-16 23:27:22 +0900

    Update isItAVowelInThisPosition.js

--- isItAVowelInThisPosition.js
+++ isItAVowelInThisPosition.js
@@ -1,12 +1,18 @@
-Check if it is a vowel(a, e, i, o, u,) on the n position in a string (the first argument). Don't forget about uppercase.
+// Check if it is a vowel(a, e, i, o, u,) on the n position in a string (the first argument). Don't forget about uppercase.
 
-A few cases:
+// A few cases:
 
-{
-checkVowel('cat', 1)  ->   true // 'a' is a vowel
-checkVowel('cat', 0)  ->   false // 'c' is not a vowel
-checkVowel('cat', 4)  ->   false // this position doesn't exist
-}
-P.S. If n < 0, return false
+// {
+// checkVowel('cat', 1)  ->   true // 'a' is a vowel
+// checkVowel('cat', 0)  ->   false // 'c' is not a vowel
+// checkVowel('cat', 4)  ->   false // this position doesn't exist
+// }
+// P.S. If n < 0, return false
 
 // My Solution:
+
+function checkVowel(string, position) {
+  // good luck
+  if (position < 0 || position > string.length - 1) return false;
+  return "aeiou".includes(string[position].toLowerCase());
+}

593bbe53042409a44572329a6c7435fb03637281
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-20 23:31:45 +0900

    Update findTheNextPerfectSquare.js

--- findTheNextPerfectSquare.js
+++ findTheNextPerfectSquare.js
@@ -10,3 +10,11 @@
 // 114 --> -1  #  because 114 is not a perfect square
 
 // My Solution:
+
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  let root = Math.sqrt(sq);
+  if (Number.isInteger(root)) {
+    return (root + 1) * (root + 1);
+  } else return -1;
+}

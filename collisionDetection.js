0a2380ae72ebc1e138ff9f3a86910e1b6a05b4ab
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-27 23:31:13 +0900

    Update collisionDetection.js

--- collisionDetection.js
+++ collisionDetection.js
@@ -6,3 +6,10 @@
 // If a collision is detected, return true. If not, return false.
 
 // My Solution:
+
+function collision(x1, y1, radius1, x2, y2, radius2) {
+  // collision?
+  let requiredDistance = radius2 + radius1;
+  let circleDistance = Math.hypot(x2 - x1, y2 - y1);
+  return circleDistance <= requiredDistance;
+}

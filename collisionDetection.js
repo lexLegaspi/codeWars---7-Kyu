aa319c14c01e62940bae6f92b8327b14b7a925a8
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-08-27 23:30:59 +0900

    Create collisionDetection.js

---
+++ collisionDetection.js
@@ -0,0 +1,8 @@
+// Create a function to determine whether or not two circles are colliding. You will be given the position of both circles in addition to their radii:
+
+// function collision(x1, y1, radius1, x2, y2, radius2) {
+//   // collision?
+// }
+// If a collision is detected, return true. If not, return false.
+
+// My Solution:

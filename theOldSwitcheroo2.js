932bab92d946743466d0674153f744cd69c70231
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-09-06 23:30:45 +0900

    Create theOldSwitcheroo2.js

---
+++ theOldSwitcheroo2.js
@@ -0,0 +1,10 @@
+// This is a follow up to my kata the old switcheroo.
+
+// Write a function that takes in a string and replaces all the letters with their respective positions in the English alphabet; e.g. 'a' is 1, 'z' is 26. The function should be case-insensitive.
+
+// 'abc'      --> '123'
+// 'ABC'      --> '123'
+// 'codewars' --> '315452311819'
+// 'abc-#@5'  --> '123-#@5'
+
+// My Solution:

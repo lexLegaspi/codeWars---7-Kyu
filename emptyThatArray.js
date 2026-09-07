8c020a526d145caa55bc7dd100878d3a718c5609
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-09-07 23:49:28 +0900

    Create emptyThatArray.js

---
+++ emptyThatArray.js
@@ -0,0 +1,7 @@
+// Today's task is to write a function empty(), which take an array as argument. This function should return the same array, but empty.
+
+// To make it even more fun, you are not allowed to use any kind of loop (nor recursion)
+
+// Good luck!
+
+// My Solution:

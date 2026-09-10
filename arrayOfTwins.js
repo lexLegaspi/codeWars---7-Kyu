58ca9c94258934df93ae4da402af8b444a008ac0
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-09-10 23:00:13 +0900

    Update arrayOfTwins.js

--- arrayOfTwins.js
+++ arrayOfTwins.js
@@ -1,5 +1,14 @@
 // You are given an array of numbers. Check if the array contains of pairs, i.e every number occurs exactly two times. If true, return true, otherwise false. Your solution should not modify the input array.
 
-
-
 // My Solution:
+
+function twins(myArray) {
+  // your code
+  let isTrue = true;
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray.filter((x) => x == myArray[i]).length !== 2) {
+      isTrue = false;
+    }
+  }
+  return isTrue;
+}

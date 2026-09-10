6fd3b2cf24ca0d43d7518237bbaaae0fe7b03810
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-09-10 22:59:10 +0900

    Create arrayOfTwins.js

---
+++ arrayOfTwins.js
@@ -0,0 +1,5 @@
+// You are given an array of numbers. Check if the array contains of pairs, i.e every number occurs exactly two times. If true, return true, otherwise false. Your solution should not modify the input array.
+
+
+
+// My Solution:

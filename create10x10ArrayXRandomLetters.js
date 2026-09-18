3fa068654aab13637e2498f1538b6bb7445224e7
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-09-18 23:44:12 +0900

    Update create10x10ArrayXRandomLetters.js

--- create10x10ArrayXRandomLetters.js
+++ create10x10ArrayXRandomLetters.js
@@ -3,3 +3,27 @@
 // Example: [ ['A', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['A', 'B', 'B', 'C', 'B', 'D', 'A', 'A', 'C', 'D'], ['A', 'A', 'A', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['C', 'A', 'B', 'C', 'B', 'C', 'D', 'B', 'C', 'A'], ['A', 'A', 'B', 'D', 'B', 'B', 'A', 'C', 'B', 'A'], ['B', 'A', 'B', 'C', 'B', 'D', 'A', 'B', 'C', 'B'], ['A', 'A', 'D', 'C', 'A', 'D', 'B', 'C', 'C', 'C'], ['B', 'A', 'B', 'C', 'B', 'D', 'B', 'C', 'C', 'D'], ['D', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['A', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ]
 
 // My Solution:
+
+function array10(){
+  function randomizer(){
+    return ['A', 'B', 'C', 'D'][Math.floor(Math.random() * 4)]
+  }
+
+  let arr = new Array(10).fill(new Array(10).fill(0))
+
+  return arr.map(x => {
+    let used = []
+
+    return x.map(y => {
+      let current = randomizer()
+
+      while (used.length < 4 && used.includes(current)) {
+        current = randomizer()
+      }
+
+      used.push(current)
+
+      return current
+    })
+  })
+}

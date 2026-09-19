3de3b679fc542677c17194d2a7eb0a174b8bf705
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-09-19 23:30:19 +0900

    Create everyPossibleSumOfTwoDigits.js

---
+++ everyPossibleSumOfTwoDigits.js
@@ -0,0 +1,14 @@
+// Given a number >= 0, return all the possible sums of two digits of it.
+
+// In the output, the pairs of digits should be ordered first left-to-right for the first digit, then left-to-right for subsequent digits.
+
+// For example, 12345: all possible sum of two digits from that number are:
+
+// [ 1 + 2, 1 + 3, 1 + 4, 1 + 5, 2 + 3, 2 + 4, 2 + 5, 3 + 4, 3 + 5, 4 + 5 ]
+// Therefore the result must be:
+
+// [ 3, 4, 5, 6, 5, 6, 7, 7, 8, 9 ]
+
+// My Solution:
+
+
